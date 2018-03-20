173629a83dc058911a4c6479cb8f5fb9b3fa44f2
Michael Lipscombe <mlipscombe@outlook.com>
2018-03-20 10:45:13 -0400

    Added

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Syncfusion.Pdf;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new PdfDocument();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
